63c80ec6cd811359ff38cb8a8497e99ecfaab68b
BamoJ <94501520+BamoJ@users.noreply.github.com>
2024-02-26 18:53:57 +0700

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,14 @@
+export default function hello() {
+	// Define the styles for the message
+	const messageStyles = [
+		'background: #ffffff',
+		'color: black',
+		'padding: 10px 20px',
+		'font-size: 12px',
+		'font-weight: bold',
+		'font-family: Helvetica, Arial, sans-serif',
+	]
+
 	const message = 'Site by: Bamo.J® => https://bamo-j.webflow.io/'
 
 	// Output the styled message to the console
